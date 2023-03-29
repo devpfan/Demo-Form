f2da03ec0be29b6be824e4fcbdf773533420b353
devpfan <pfabian@outlook.com>
2023-03-29 11:32:37 -0500

    estilos de prueba

--- src/main/java/com/example/demo/Contacto.java
+++ src/main/java/com/example/demo/Contacto.java
@@ -1,5 +1,31 @@
 package com.example.demo;
 
 public class Contacto {
-    
+    private String nombre;
+    private String email;
+    private String mensaje;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
 }
